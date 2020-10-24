86303e7b16bea6f4331d494be1202a1a53ca07bc
eightbite <71624120+eightbite@users.noreply.github.com>
2020-10-24 21:39:23 +0900

    バリデーションの指定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,20 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  # with_options presence: true do
-  #   validates :name
-  #   validates :exposition
-  #   validates :category_id
-  #   validates :status_id
-  #   validates :burden_id
-  #   validates :area_id
-  #   validates :day_id
-  #   validates :price
-  # end
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :exposition
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
+    validates :price, 
+              numericality: {
+                only_integer: true,
+                greater_than_or_equal_to: 300,
+                less_than_or_equal_to: 9999999
+              }
+  end
 end
