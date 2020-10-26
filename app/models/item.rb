cea108062facd68dc0b3fdc24c256b535c4b24ea
eightbite <71624120+eightbite@users.noreply.github.com>
2020-10-26 23:42:05 +0900

    商品詳細ページにおける商品情報の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
   has_one_attached :image
   belongs_to :user
 
