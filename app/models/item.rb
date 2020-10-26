2a4119c21edc8cc9a8a3d0de2df4f4492da37482
eightbite <71624120+eightbite@users.noreply.github.com>
2020-10-26 15:17:57 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,16 +7,18 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :exposition
-    validates :category_id
-    validates :status_id
-    validates :burden_id
-    validates :area_id
-    validates :day_id
     validates :price, 
               numericality: {
                 only_integer: true,
                 greater_than_or_equal_to: 300,
                 less_than_or_equal_to: 9999999
               }
+  end
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
   end
 end
