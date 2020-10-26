a6d467b031a8ea7c3963e8e39243fe191f470bf1
eightbite <71624120+eightbite@users.noreply.github.com>
2020-10-26 15:57:06 +0900

    不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
 
-  resources :items, only: [:index, :new, :create] do
-    resources :articles
-  end
+  resources :items, only: [:index, :new, :create]
 end
