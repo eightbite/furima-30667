fbaa9deb3df8b7603d7d87cc37c083d131a22265
eightbite <71624120+eightbite@users.noreply.github.com>
2020-10-27 14:59:10 +0900

    記述の誤りを訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user.id
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
